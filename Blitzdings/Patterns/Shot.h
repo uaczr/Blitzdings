2d548b64a2618420134a512b73a2e50884dd5c28
uaczr <uaczr@student.kit.edu>
2015-03-27 20:57:29 +0100

    shot

---
+++ Blitzdings/Patterns/Shot.h
@@ -0,0 +1,16 @@
+/*
+ * Shot.h
+ *
+ *  Created on: 27.03.2015
+ *      Author: Christoph
+ */
+#include <Arduino.h>
+#include <FastLED.h>
+
+#ifndef PATTERNS_SHOT_H_
+#define PATTERNS_SHOT_H_
+
+extern void shot();
+
+
+#endif /* PATTERNS_SHOT_H_ */
