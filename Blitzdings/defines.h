faaecf6dcb279e71e6cbf57530b991453e1c4cea
uaczr <uaczr@student.kit.edu>
2015-04-08 12:09:55 +0200

    defines.h --> Parameternamen

---
+++ Blitzdings/defines.h
@@ -0,0 +1,44 @@
+/*
+ * defines.h
+ *
+ *  Created on: 06.04.2015
+ *      Author: Christoph
+ */
+
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+//Parameterfeld
+#define EVENTS parameter[0]
+#define PATTERNS parameter[1]
+#define BPM parameter[2]
+#define GPARAM3 parameter[3]
+#define P1_TYPE parameter[4]
+#define P1_TIME parameter[5]
+#define P1_COLOR parameter[6]
+#define P2_PARAM1 parameter[7]
+#define P2_PARAM2 parameter[8]
+#define P2_PARAM3 parameter[9]
+#define P3_PARAM1 parameter[10]
+#define P3_PARAM2 parameter[11]
+#define P3_PARAM3 parameter[12]
+#define P4_PARAM1 parameter[13]
+#define P4_PARAM2 parameter[14]
+#define P4_PARAM3 parameter[15]
+#define P5_PARAM1 parameter[16]
+#define P5_PARAM2 parameter[17]
+#define P5_PARAM3 parameter[18]
+#define P6_PARAM1 parameter[19]
+#define P6_PARAM2 parameter[20]
+#define P6_PARAM3 parameter[21]
+#define P7_PARAM1 parameter[22]
+#define P7_PARAM2 parameter[23]
+#define P7_PARAM3 parameter[24]
+#define P8_PARAM1 parameter[25]
+#define P8_PARAM2 parameter[26]
+#define P8_PARAM3 parameter[27]
+
+
+//ColorFeld
+
+#endif /* DEFINES_H_ */
